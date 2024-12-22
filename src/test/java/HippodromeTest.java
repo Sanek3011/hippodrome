551f62ae8f9c5467beb8b14055dfda64b44aeeb3
Alexandr Zaikin <sanekzkz@gmail.com>
2024-12-22 17:51:07 +0300

    Исправил тест moveHippodromeTest

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.mockito.Mockito;
 
@@ -11,25 +10,31 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HippodromeTest {
-    @ParameterizedTest
-    @NullSource
-    void hippodromeConstructorWithNullTest(List<Horse> horses) {
-        IllegalArgumentException illegal = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new Hippodrome(horses);
-        });
-        assertEquals("Horses cannot be null.", illegal.getMessage());
-
-    }
     @Test
-    void hippodromeConstructorEmptyListTest() {
-        IllegalArgumentException illegal = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new Hippodrome(new ArrayList<>());
-        });
-        assertEquals("Horses cannot be empty.", illegal.getMessage());
-
+    void hippodromeConstructorWithNullAndEmptyListTest() {
+        assertAll(
+                () -> {
+                    IllegalArgumentException illegal = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+                    assertEquals("Horses cannot be null.", illegal.getMessage());
+                },
+                () -> {
+                    IllegalArgumentException illegal = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(new ArrayList<>()));
+                    assertEquals("Horses cannot be empty.", illegal.getMessage());
+                }
+        );
 
 
     }
+//    @Test
+//    void hippodromeConstructorEmptyListTest() {
+//        IllegalArgumentException illegal = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+//            new Hippodrome(new ArrayList<>());
+//        });
+//        assertEquals("Horses cannot be empty.", illegal.getMessage());
+//
+//
+//
+//    }
 
 
     @Test
@@ -39,7 +44,7 @@ class HippodromeTest {
         assertEquals(horses, hippo.getHorses());
     }
 
-    List<Horse> getListOfHorses(int capacity, boolean isMock) {
+    private List<Horse> getListOfHorses(int capacity, boolean isMock) {
         List<Horse> horses = new ArrayList<>();
         for (int i = 0; i < capacity; i++) {
             if (isMock) {
@@ -57,18 +62,18 @@ class HippodromeTest {
         Hippodrome hippo = new Hippodrome(mockHorses);
         hippo.move();
         for (Horse horse : mockHorses) {
-            Mockito.verify(horse).move();
+            Mockito.verify(horse, Mockito.times(1)).move();
         }
     }
 
     @Test
     void getWinnerTest() {
         List<Horse> horses = new ArrayList<>();
-        Horse horse1 = new Horse("Horsyashak", 10, 10);
+        Horse horse1 = new Horse("Horsyashka", 10, 10);
         horses.add(horse1);
         Horse horse2 = new Horse("Bingo", 10, 25);
         horses.add(horse2);
-        Horse horse3 = new Horse("Wind", 10, 26);
+        Horse horse3 = new Horse("Wingo", 10, 26);
         horses.add(horse3);
         Hippodrome hippodrome = new Hippodrome(horses);
         assertSame(horse3, hippodrome.getWinner());
